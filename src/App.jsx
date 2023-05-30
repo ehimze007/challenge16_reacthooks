53e7ca08e785733d87c2f4d367b42f050bc367a7
Michael Peter <michaelhpet@gmail.com>
2023-05-30 21:03:49 +0100

    feat: implement function to add new movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,57 @@
+import { useState } from "react";
 import "./App.css";
 import MovieList from "./components/MovieList";
-import movies from "./movies";
+import moviesList from "./movies";
 
 function App() {
+  const [movies, setMovies] = useState(moviesList);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const movie = {
+      title: form.title.value,
+      description: form.description.value,
+      posterUrl: form.posterUrl.value,
+      rating: parseInt(form.rating.value),
+    };
+
+    setMovies((prev) => [...prev, movie]);
+  }
+
   return (
     <div className="app">
       <aside>
         <h1>Add a new movie</h1>
-        <form action="">
-          <input type="text" placeholder="Movie title" />
+        <form onSubmit={handleSubmit}>
+          <input name="title" type="text" placeholder="Movie title" required />
           <textarea
+            name="description"
             type="text"
             rows={5}
             placeholder="Movie description"
+            required
           ></textarea>
-          <input type="text" placeholder="Movie poster url" />
-          <input type="number" placeholder="Movie rating" />
-          <button>Add movie</button>
+          <input
+            name="posterUrl"
+            type="text"
+            placeholder="Movie poster url"
+            required
+          />
+          <input
+            name="rating"
+            type="number"
+            placeholder="Movie rating"
+            required
+          />
+          <button type="submit">Add movie</button>
         </form>
       </aside>
+
       <main>
-        <form action="">
+        <form>
           <input type="text" placeholder="Search" />
         </form>
 
